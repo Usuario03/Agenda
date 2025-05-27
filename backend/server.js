4c30e8a4d891d9e550162ecc994577b9c00cd279
Usuario03 <jaleal524@gmail.com>
2025-05-27 13:28:25 -0500

    Corrige mensaje de puerto dinámico en consola

--- backend/server.js
+++ backend/server.js
@@ -15,5 +15,5 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 
 app.listen(5000, () => {
-  console.log("Servidor corriendo en http://localhost:5000");
+ console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
